219020df203e128ea70112e78710904b48b49b4a
Kirill <kirill@on-life.io>
2020-05-13 01:27:13 +0300

    initial

--- TopMusic/MainTabBarController.swift
+++ TopMusic/MainTabBarController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        let searchVC = SearchViewController()
+        let libraryVC = ViewController()
+        
+        viewControllers = [
+            generateViewController(rootViewController: searchVC, image: #imageLiteral(resourceName: "search"), title: "Search"),
+            generateViewController(rootViewController: libraryVC, image: #imageLiteral(resourceName: "library"), title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage,
+                                        title: String) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
+        
+    }
+    
+}
--- TopMusic/SearchViewController.swift
+++ TopMusic/SearchViewController.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        <#code#>
+    }
+}
