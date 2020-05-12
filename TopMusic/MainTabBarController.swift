2c690c01ab1da85fe648075797cbf8ae3abb9b10
Kirill <kirill@on-life.io>
2020-05-13 01:21:53 +0300

    initial

---
+++ TopMusic/MainTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarController.swift
+//  TopMusic
+//
+//  Created by Кирилл Медведев on 13.05.2020.
+//  Copyright © 2020 Kirill Medvedev. All rights reserved.
+//
+
+import Foundation
---
+++ TopMusic/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  TopMusic
+//
+//  Created by Кирилл Медведев on 13.05.2020.
+//  Copyright © 2020 Kirill Medvedev. All rights reserved.
+//
+
+import Foundation
